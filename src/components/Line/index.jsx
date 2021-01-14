781b1a2ee1629a8d03fabe5b237a4988d8552fc2
Lujain <lujain.abdallatif@mail.huji.ac.il>
2021-01-14 15:38:51 +0200

    react icons

---
+++ src/components/Line/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NetIcon } from "../../icons";
+
+export default function Line(props) {
+  return (
+    <div>
+      <NetIcon />
+    </div>
+  );
+}
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useContext, useEffect } from "react";
+import Line from "../../components/Line";
 
 const Home = (props) => {
-  return <div> Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div>
+        <Line />
+      </div>
+    </div>
+  );
 };
 export default Home;
