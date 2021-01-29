ae16190ce9032261bf7e170f3daed10ae365ad6e
OmklthomAmara <amaraomklthom@gmail.com>
2021-01-30 00:07:43 +0200

    examinee flow

---
+++ src/components/Alert/index.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const Alert = () => {
+  return <h1 className="Alert-Div"> Read the Question and try Again !!</h1>;
+};
+
+export default Alert;
